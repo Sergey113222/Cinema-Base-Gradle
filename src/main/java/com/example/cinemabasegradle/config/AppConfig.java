d71cde9c4fb94eeb20dbefe6b74739bf9a8920df
Gritsuk_Sergey <k-grine@mail.ru>
2021-10-13 10:37:30 +0300

    add sql-query.properties, application-jdbc.properties, application-jpa.properties, create bean with sql-query.properties

--- src/main/java/com/example/cinemabasegradle/config/AppConfig.java
+++ src/main/java/com/example/cinemabasegradle/config/AppConfig.java
@@ -5,11 +5,13 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@PropertySource("classpath:sql-query.properties")
 public class AppConfig {
 
     @Bean
