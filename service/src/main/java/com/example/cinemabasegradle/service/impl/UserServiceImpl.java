7be3992aeb27b4c2771a1356f0bd6cc588157fb7
Gritsuk_Sergey <k-grine@mail.ru>
2022-02-18 12:35:15 +0300

    pull request update

--- service/src/main/java/com/example/cinemabasegradle/service/impl/UserServiceImpl.java
+++ service/src/main/java/com/example/cinemabasegradle/service/impl/UserServiceImpl.java
@@ -43,12 +43,10 @@ public class UserServiceImpl implements UserService {
 
         List<Role> roles = user.getRoles();
         List<Role> rolesFromDB = new ArrayList<>();
-        if (!roles.contains(ROLE_ADMIN)) {
-            for (Role role : roles) {
+        for (Role role : roles) {
+            if (!role.getName().equals(ROLE_ADMIN)) {
                 rolesFromDB.add(roleRepository.findByName(role.getName()));
-            }
-        } else {
-            throw new IllegalArgumentException(MESSAGE);
+            } else throw new IllegalArgumentException(MESSAGE);
         }
         user.setRoles(rolesFromDB);
         Profile profile = user.getProfile();
